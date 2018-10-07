b9ad0d8a568491a628ca0a9a35045dc0b6727e74
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2018-10-07 20:52:34 +0530

    added code for UI of the calculator

---
+++ app/src/main/java/com/example/dhirajchhabraeng/calculatorlatest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.dhirajchhabraeng.calculatorlatest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.tool_bar_main_screen);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+}
